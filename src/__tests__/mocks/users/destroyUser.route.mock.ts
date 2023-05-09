22ff9cb0db9d7123f7226ef169b0d45800669f3b
Hanna Boppe <105255009+hboppe@users.noreply.github.com>
2023-05-09 09:11:49 -0400

    first commit

--- src/__tests__/mocks/users/destroyUser.route.mock.ts
+++ src/__tests__/mocks/users/destroyUser.route.mock.ts
@@ -2,13 +2,13 @@ export default {
   userAdminTemplate: {
     name: 'Fabio',
     email: 'fabio@kenzie.com.br',
-    password: '1234',
+    password: 'Senh@123',
     admin: true,
   },
   userNotAdminTemplate: {
     name: 'Cauan',
     email: 'cauan@kenzie.com.br',
-    password: '1234',
+    password: 'Senh@123',
     admin: false,
   },
 };
